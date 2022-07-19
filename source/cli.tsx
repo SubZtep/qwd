bc764e166f4c4f58b117869a88bfd50da1a8dd7f
Andras Serfozo <subztep@gmail.com>
2022-07-19 14:09:15 +0100

    Reset project to virgin Ink

---
+++ source/cli.tsx
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+import React from 'react';
+import {render} from 'ink';
+import meow from 'meow';
+import App from './ui';
+
+const cli = meow(`
+	Usage
+	  $ qwd
+
+	Options
+		--name  Your name
+
+	Examples
+	  $ qwd --name=Jane
+	  Hello, Jane
+`, {
+	flags: {
+		name: {
+			type: 'string'
+		}
+	}
+});
+
+render(<App name={cli.flags.name}/>);
